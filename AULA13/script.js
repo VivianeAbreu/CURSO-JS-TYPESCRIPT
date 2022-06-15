ed90891853169332cadbbdcffbaaf49b60edab99
Viviane Abreu <vivianevieiraleme@gmail.com>
2022-06-15 15:25:52 -0300

    exercicios

---
+++ AULA13/script.js
@@ -0,0 +1,9 @@
+let num1 = prompt('Digite um número');
+let num2 = prompt('Digite outro valor');
+
+num1 = Number(num1);
+num2 = Number(num2);
+
+
+
+alert(`O resultado da sua conta foi: ${num1 + num2}` ); 
