07a5a57331a6ae6077fc8055d5cf4ea40e8baa1a
Viviane Abreu <vivianevieiraleme@gmail.com>
2022-06-15 10:38:07 -0300

    operadores aritméticos

--- AULA12/index.js
+++ AULA12/index.js
@@ -1,14 +1,22 @@
 // Contatenação = unir dois valores
 // incremento = sao os operados ++
 // decremento = sao os operadores -- 
+// NaN -  not a number parseInt (inteiro), parseFloat (decimais)
 /**
  * Aritméticos
+ * Operadores de Atribuição
  *  + Adição / Concatenação
  *  potenciaçã0  ** exemplo 2 elevado a 10
  * % resto da divisão
  */
 
-
 const num1 = 5;
 const num2 = 10;
 console.log(num1 + num2);
+
+let contador = 2;
+contador **=10;
+console.log(contador);
+
+
+
