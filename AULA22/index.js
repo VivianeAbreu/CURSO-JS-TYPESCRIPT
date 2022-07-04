98b02528823f0b4bf55c45b376ba4a4a885532e3
Viviane Abreu <vivianevieiraleme@gmail.com>
2022-07-04 09:37:13 -0300

    operados lógicos

---
+++ AULA22/index.js
@@ -0,0 +1,17 @@
+/*
+Operadores Lógicos
+&& -> AND -> E -> Todas as expressões ser verdadeiras para retornar true
+|| -> OR -> OU
+! -> NOT -> NÃO
+*/
+// const expressaoAnd = true && true && false && true;
+// const expressaoOr = true || false;
+
+// const usuario = 'Luiz'; // form que o usuario digitou
+// const senha = '123456'; // form que o usuario digitou
+
+    //                     true                false   
+// const vailogar = usuario === 'Luiz' && senha === '12345';
+// console.log(vailogar);
+
+console.log(!true);
