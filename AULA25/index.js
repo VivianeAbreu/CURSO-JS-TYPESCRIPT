55873407d900bbad56a29ce93e770eb1b746ed5b
Viviane Abreu <vivianevieiraleme@gmail.com>
2022-07-05 15:07:13 -0300

    objeto Date

---
+++ AULA25/index.js
@@ -0,0 +1,17 @@
+// condicao ? 'Valor para verdadeiro' : 'Valor para falso'
+const pontuacaoUsuario = 500;
+const nivelUsuario = pontuacaoUsuario >= 1000 ? 'Usuario VIP' : 'Usuario normal';
+
+
+
+const corUsurario = null;
+const corPadrao = corUsurario || 'Preta';
+
+console.log(nivelUsuario, corPadrao);
+
+
+//if(pontuacaoUsuario >= 1000) {
+//   console.log('Usuario VIP');
+//} else {
+//  console.log('Usuario normal');
+// }
